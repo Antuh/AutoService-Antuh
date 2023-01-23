092c8c79a7284bc2b8e871ed3f5a9732adb854ae
Antuh <antuhsergey648@gmail.com>
2023-01-23 22:13:13 +0700

    Initial two commit

--- AutoService. Antuh/View/Authorization.xaml.cs
+++ AutoService. Antuh/View/Authorization.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using AutoService.Antuh.Model;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,6 +35,7 @@ namespace AutoService.Antuh.View
             var authorization = m.User;
 
 
+
             var user = authorization.Where(x => x.UserLogin == login && x.UserPassword == password).FirstOrDefault();
             if (user != null)
             {
@@ -52,23 +54,13 @@ namespace AutoService.Antuh.View
                 TimeError f = new TimeError();
                 f.Show();
                 this.Close();
-                /*MessageBox.Show("Такого пользователя не существует");
-                String allowchar = " ";
-                allowchar = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
-                allowchar += "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,y,z";
-                allowchar += "1,2,3,4,5,6,7,8,9,0";
-                char[] a = { ',' };
-                String[] ar = allowchar.Split(a);
-                String pwd = " ";
-                string temp = " ";
-                Random r = new Random();
-                for (int i = 0; i < 6; i++)
-                {
-                    temp = ar[(r.Next(0, ar.Length))];
-                    pwd += temp;
-                }
-                tb_capcha.Text = pwd;*/
+                
             }
+        }
+        private void btn_seeclient_Click(object sender, RoutedEventArgs e)
+        {
+            Client f = new Client();
+            f.Show();
         }
     }
 }
---
+++ AutoService. Antuh/View/Client.xaml.cs
@@ -0,0 +1,41 @@
+﻿using AutoService.Antuh.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutoService.Antuh.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Client.xaml
+    /// </summary>
+    public partial class Client : Window
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+        Entities m = new Entities();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query =
+            from product in m.Product
+            select new { product.ProductName, product.ProductDescription };
+
+            dataGrid1.ItemsSource = query.ToList();
+        }
+    }
+}
+
+
+
